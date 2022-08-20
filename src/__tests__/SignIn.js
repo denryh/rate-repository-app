9525fea94d0caa6dac398245dada939f69415771
poitrew <dtdat101101@gmail.com>
2022-08-20 19:58:53 +0700

    added test for sign in form (10.18)

---
+++ src/__tests__/SignIn.js
@@ -0,0 +1,20 @@
+import { render, fireEvent, waitFor } from "@testing-library/react-native";
+
+import { SignInContainer } from "../components/SignIn";
+
+describe("SignIn", () => {
+    describe("SignInContainer", () => {
+        it("calls onSubmit function with correct arguments when a valid form is submitted", async () => {
+            const onSubmit = jest.fn();
+            const { getByText, getByPlaceholderText } = render(<SignInContainer onSubmit={onSubmit}/>);
+
+            fireEvent.changeText(getByPlaceholderText("Username"), "kalle");
+            fireEvent.changeText(getByPlaceholderText("Password"), "password");
+            fireEvent.press(getByText("Sign in"));
+
+            await waitFor(() => {
+                expect(onSubmit).toHaveBeenCalledTimes(1);
+            });
+        });
+    });
+});
--- src/components/SignIn.jsx
+++ src/components/SignIn.jsx
@@ -1,97 +1,101 @@
-import { Pressable, StyleSheet, View } from 'react-native'
-import { Formik } from 'formik'
-import * as yup from 'yup'
+import { Pressable, StyleSheet, View } from "react-native";
+import { Formik } from "formik";
+import * as yup from "yup";
 
-import { useNavigate } from 'react-router-native'
-import useSignIn from '../hooks/useSignIn'
+import { useNavigate } from "react-router-native";
+import useSignIn from "../hooks/useSignIn";
 
-import FormikTextInput from './FormikTextInput'
-import Text from './Text'
-import theme from '../theme'
+import FormikTextInput from "./FormikTextInput";
+import Text from "./Text";
+import theme from "../theme";
 
 const initialValues = {
-  username: '',
-  password: '',
-}
+    username: "",
+    password: "",
+};
 
 const validationSchema = yup.object().shape({
-  username: yup
-    .string()
-    .min(3, 'Username must have a min length of 3')
-    .required('Username is required'),
-  password: yup
-    .string()
-    .min(6, 'Password must have a min length of 6')
-    .required('Password is required')
-})
+    username: yup
+        .string()
+        .min(3, "Username must have a min length of 3")
+        .required("Username is required"),
+    password: yup
+        .string()
+        .min(6, "Password must have a min length of 6")
+        .required("Password is required")
+});
+
+export const SignInContainer = ({ onSubmit }) => {
+    return <Formik 
+        initialValues={initialValues} 
+        onSubmit={onSubmit} 
+        validationSchema={validationSchema}
+    >
+        {({ handleSubmit }) => <SignInForm onSubmit={handleSubmit} />}
+    </Formik>;
+};
 
 const SignIn = () => {
-  const navigate = useNavigate()
-  const [signIn] = useSignIn()
+    const navigate = useNavigate();
+    const [signIn] = useSignIn();
 
-  const onSubmit = async (values) => {
-    const { username, password } = values
+    const onSubmit = async (values) => {
+        const { username, password } = values;
 
-    try {
-      const data = await signIn({ username, password });
-      navigate("/")
-      console.log(data)
-    } catch (e) {
-      console.log(e)
-    }
-  };
+        try {
+            const data = await signIn({ username, password });
+            navigate("/");
+            console.log(data);
+        } catch (e) {
+            console.log(e);
+        }
+    };
 
-  return <Formik 
-      initialValues={initialValues} 
-      onSubmit={onSubmit} 
-      validationSchema={validationSchema}
-    >
-    {({ handleSubmit }) => <SignInForm onSubmit={handleSubmit} />}
-  </Formik>
+    return <SignInContainer onSubmit={onSubmit} />;
 };
 
 const styles = StyleSheet.create({
-  container: {
-    paddingHorizontal: 10,
-    paddingBottom: 10,
-    backgroundColor: 'white',
-    alignItems: 'stretch'
-  },
-  textInput: {
-    marginTop: 10,
-    padding: 15,
-    borderColor: theme.colors.textSecondary,
-    borderStyle: 'solid',
-    borderWidth: 1,
-    borderRadius: 3,
-  },
-  button: {
-    marginTop: 10,
-    backgroundColor: theme.colors.primary,
-    borderRadius: 3,
-    padding: 15,
-  }
-})
+    container: {
+        paddingHorizontal: 10,
+        paddingBottom: 10,
+        backgroundColor: "white",
+        alignItems: "stretch"
+    },
+    textInput: {
+        marginTop: 10,
+        padding: 15,
+        borderColor: theme.colors.textSecondary,
+        borderStyle: "solid",
+        borderWidth: 1,
+        borderRadius: 3,
+    },
+    button: {
+        marginTop: 10,
+        backgroundColor: theme.colors.primary,
+        borderRadius: 3,
+        padding: 15,
+    }
+});
 
 const SignInForm = ({ onSubmit }) => {
-  return <View style={styles.container}>
-    <FormikTextInput 
-      style={styles.textInput} 
-      name="username" 
-      placeholder="Username" 
-      placeholderTextColor={theme.colors.textSecondary} 
-    />
-    <FormikTextInput 
-      style={styles.textInput} 
-      name="password" 
-      placeholder="Password" 
-      placeholderTextColor={theme.colors.textSecondary} 
-      secureTextEntry 
-    />
-    <Pressable style={styles.button} onPress={onSubmit}>
-      <Text fontWeight='bold' style={{ textAlign: 'center', color: 'white' }}>Sign in</Text>
-    </Pressable>
-  </View>
-}
+    return <View style={styles.container}>
+        <FormikTextInput 
+            style={styles.textInput} 
+            name="username" 
+            placeholder="Username" 
+            placeholderTextColor={theme.colors.textSecondary} 
+        />
+        <FormikTextInput 
+            style={styles.textInput} 
+            name="password" 
+            placeholder="Password" 
+            placeholderTextColor={theme.colors.textSecondary} 
+            secureTextEntry 
+        />
+        <Pressable style={styles.button} onPress={onSubmit}>
+            <Text fontWeight='bold' style={{ textAlign: "center", color: "white" }}>Sign in</Text>
+        </Pressable>
+    </View>;
+};
 
 export default SignIn;
